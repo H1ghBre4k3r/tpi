2990c7efe2c9c54e6bf1b3165bb7929dd25cedab
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2023-05-15 18:13:39 +0200

    Add implementations for Ping

--- src/bin/tpi-server/main.rs
+++ src/bin/tpi-server/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use tonic::{Response, Status, Request, transport::Server};
+use turing::{turing_pi_server::{TuringPi, TuringPiServer}, PingResponse};
+
+pub mod turing {
+    tonic::include_proto!("turing");
+}
+
+#[derive(Debug)]
+struct TpiServer;
+
+#[tonic::async_trait]
+impl TuringPi for TpiServer {
+    async fn ping(
+        &self,
+        request: Request<turing::PingRequest>,
+    ) -> Result<Response<PingResponse>, Status>
+    {
+        Ok(Response::new(PingResponse { }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    let addr = "0.0.0.0:50051".parse()?;
+
+    let tpi_server = TpiServer;
+
+    Server::builder()
+        .add_service(TuringPiServer::new(tpi_server))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
--- src/bin/tpi/main.rs
+++ src/bin/tpi/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use tonic::Request;
+use turing::{turing_pi_client::TuringPiClient, PingRequest};
+
+pub mod turing {
+    tonic::include_proto!("turing");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut client = TuringPiClient::connect("http://192.168.2.160:50051").await?;
+
+    let request = Request::new(PingRequest {});
+
+    let response = client.ping(request).await?;
+
+    Ok(())
 }
